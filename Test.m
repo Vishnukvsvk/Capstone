030fedd4de25cd99ecb971ab4972cf561af5de00
Kvs Vishnu Kumar <36696407+Vishnukvsvk@users.noreply.github.com>
2021-02-27 14:57:11 +0530

    Create Test.m

---
+++ Test.m
@@ -0,0 +1,21 @@
+fprintf('%s - Classifying test frames\n', datestr(toc/86400,'HH:MM:SS'))
+
+% Gather the test frames into the memory
+testFramesTall = tall(transform(testDSTrans, @helperModClassReadFrame));
+rxTestFrames = gather(testFramesTall);
+
+rxTestFrames = cat(4, rxTestFrames{:});
+
+% Gather the test labels into the memory
+testLabelsTall = tall(transform(testDSTrans, @helperModClassReadLabel));
+rxTestLabels = gather(testLabelsTall);
+
+rxTestPred = classify(trainedNet,rxTestFrames);
+testAccuracy = mean(rxTestPred == rxTestLabels);
+disp("Test accuracy: " + testAccuracy*100 + "%")
+
+figure
+cm = confusionchart(rxTestLabels, rxTestPred);
+cm.Title = 'Confusion Matrix for Test Data';
+cm.RowSummary = 'row-normalized';
+cm.Parent.Position = [cm.Parent.Position(1:2) 740 424];
