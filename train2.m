9b01f88fa9d3c46bc14ba6b8d1beb8966127385d
Kvs Vishnu Kumar <36696407+Vishnukvsvk@users.noreply.github.com>
2021-03-19 14:16:03 +0530

    Create train2.m

---
+++ train2.m
@@ -0,0 +1,40 @@
+dataDirectory = "mycnn";
+percentTrainingSamples = 80;
+percentValidationSamples = 10;
+percentTestSamples = 10;
+modulationTypes = categorical(["BPSK", "QPSK", "8PSK", "16QAM", "64QAM", "256QAM","1024QAM","PAM4", "GFSK", "CPFSK","DSB-AM","SSB-AM","B-FM"]);
+
+%%%%%%%%%%%
+
+frameDS = signalDatastore(dataDirectory,'SignalVariableNames',["frame","label"]);
+frameDSTrans = transform(frameDS,@helperModClassIQAsPages);
+
+splitPercentages = [percentTrainingSamples,percentValidationSamples,percentTestSamples];
+[trainDSTrans,validDSTrans,testDSTrans] = helperModClassSplitData(frameDSTrans,splitPercentages);
+
+trainFramesTall = tall(transform(trainDSTrans, @helperModClassReadFrame));
+rxTrainFrames = gather(trainFramesTall);
+
+rxTrainFrames = cat(4, rxTrainFrames{:});
+validFramesTall = tall(transform(validDSTrans, @helperModClassReadFrame));
+rxValidFrames = gather(validFramesTall);
+
+rxValidFrames = cat(4, rxValidFrames{:});
+
+% Gather the training and validation labels into the memory
+trainLabelsTall = tall(transform(trainDSTrans, @helperModClassReadLabel));
+rxTrainLabels = gather(trainLabelsTall);
+
+validLabelsTall = tall(transform(validDSTrans, @helperModClassReadLabel));
+rxValidLabels = gather(validLabelsTall);
+
+modClassNet = helperModClassCNN(modulationTypes,sps,spf);
+
+maxEpochs = 12;
+miniBatchSize = 256;
+options = helperModClassTrainingOptions(maxEpochs,miniBatchSize,...
+  numel(rxTrainLabels),rxValidFrames,rxValidLabels);
+
+
+fprintf('%s - Training the network\n', datestr(toc/86400,'HH:MM:SS'))
+[trainedNet,info] = trainNetwork(rxTrainFrames,rxTrainLabels,modClassNet,options);
